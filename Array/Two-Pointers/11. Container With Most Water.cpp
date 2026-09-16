1acc71144698f5327bb911a8cc7bb4f2d1d85331
Archit <architsv027@gmail.com>
2026-09-16 11:30:52 +0530

    Implement two-pointer approach for max area calculation
    Added a two-pointer solution for the 'Container With Most Water' problem, improving the algorithm's efficiency.

--- Array/Two-Pointers/11. Container With Most Water.cpp
+++ Array/Two-Pointers/11. Container With Most Water.cpp
@@ -1,6 +1,45 @@
 // Add two pointer solution for Container With Most Water
 
 
+
+
+//first Approach(Brute Force)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    // Brute Force Approach
+    // Time: O(n^2)
+    // Space: O(1)
+
+    int maxAreaBruteForce(vector<int>& height) {
+
+        int ans = 0;
+
+        for (int i = 0; i < height.size(); i++) {
+
+            for (int j = i + 1; j < height.size(); j++) {
+
+                int width = j - i;
+                int h = min(height[i], height[j]);
+
+                int area = width * h;
+
+                ans = max(ans, area);
+            }
+        }
+
+        return ans;
+    }
+
+
+
+// Second and optimal approch
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -33,5 +72,18 @@ public:
     }
 };
 
+// Optimized Two Pointer Approach
+    // Time: O(n)
+    // Space: O(1)
 
-//
+
+
+
+
+
+
+
+
+
+
+
